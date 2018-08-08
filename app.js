ea59d14d0772ebbe94e8fd60c2ddbc06b3e61567
robert wynn <wynncreations@gmail.com>
2018-08-08 17:46:16 -0500

    added a bunch of npm packages to project

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 var express = require("express");
 var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+
+
+//turn on server
+app.listen("3000", "localhost", function () {
+    console.log("SocialSquared Sociably Connected");
+});
