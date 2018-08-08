41323fd47db236fabfd9acfd88eceb47accdd33a
robert wynn <wynncreations@gmail.com>
2018-08-08 17:49:45 -0500

    added default route.

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
-
+app.get("/",function(req,res){
+    res.send("SocialSquare - Coming Soon!");
+});
 
 //turn on server
 app.listen("3000", "localhost", function () {
