f0273a00910d7319735ea98da6861b2015b905de
robert wynn <wynncreations@gmail.com>
2018-08-08 22:57:32 -0500

    whitespace

--- public/js/misc.js
+++ public/js/misc.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    
+    
     //make sure password and confirm password on the register page are the same before allowing submission
     $("[type=password]").on("change",function () {
         if ($("#password").val() !== $("#conpw").val() || ($("#password").val().length < 8)) {
