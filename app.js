c79b6319a7e415dfc4024a8f5d4fa6ac5d12b11f
robert wynn <wynncreations@gmail.com>
2018-08-22 08:44:33 -0500

    updated npm, added register route and DB addition for user.

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-var express = require("express"),
-    expressSession = require("express-session");
-    app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    passport = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override");
+var express         = require("express"),
+    expressSession  = require("express-session");
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    methodOverride  = require("method-override");
+    User            = require("./models/user");
+    flash           = require("connect-flash");
 
 require("dotenv").config();
     
-
 app.use(express.static(__dirname + "/public"));
 
 app.get("/",function(req,res){
     res.render("landing");
-});
+})
 
 // var commentRoutes = require("./routes/comments"),
 //     campgroundRoutes = require("./routes/campgrounds"),
@@ -33,16 +34,17 @@ var loginRoutes = require("./routes/login");
 app.use(loginRoutes);
 
 
-// mongoose.connect(process.env.DBPRODURL, options, function () {
-//     //mongoose.connection.db.dropDatabase();
-// });
+ mongoose.connect(process.env.DBPRODURL, options, function () {
+     //mongoose.connection.db.dropDatabase();
+ });
 
 //Connection to db.
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function () {
-//     // we're connected!
-// });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+});
+
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
@@ -50,7 +52,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(__dirname + "/public"));
 
-// app.use(flash()); //include flash config so we can do     some messages
+ app.use(flash()); //include flash config so we can do     some messages
 
 //seedDB();
 //Passort config
@@ -62,9 +64,9 @@ app.use(require("express-session")({
 
 app.use(passport.initialize());
 app.use(passport.session());
- //passport.use(new LocalStrategy(User.authenticate()));
- //passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //turn on server
 app.listen("3000", "localhost", function () {
--- routes/register.js
+++ routes/register.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var User = require("../models/user");
+
 var router = express.Router({
     mergeParams: true
 });
@@ -7,6 +9,29 @@ router.get("/register",function(req,res){
     res.render("register");
 });
 
+//User is registering.
+router.post("/register",function(req,res){
+    //Check if username is already in use
+    User.find({email:req.params.email},(err,user)=>{
+        if(err){
+            console.log("Error connecting to DB: ",err);
+        }else{
+            if(!user){//No user found. Lets save.
+                User.save(req.params.user);
+            }else{//Found a user, redirect
+                res.redirect(302,"/register");
+            }
+        }
+    });
+
+
+
+
+
+    //return to registration page with error.
+
+    //If no user by this email, lets add to DB.
+});
 
 
 
