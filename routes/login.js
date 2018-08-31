0577459b5e2e0727e2276ac6c45b3c8cbb03a0ef
robert wynn <wynncreations@gmail.com>
2018-08-31 14:36:18 -0500

    removed unneeded user model.

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 var express     = require("express");
 var passport    = require("passport");
 const middlware = require("../middleware");
+const User      = require("../models/user");
 var router      = express.Router({
     mergeParams: true
 });
@@ -13,11 +14,13 @@ router.post("/login", passport.authenticate("local", {
                 successRedirect: "/",
                 failureRedirect: "/login"
             }),function(req,res){
+                //res.redirect("/login");
 });
 
 router.get("/logout",middlware.isLoggedIn,(req,res,next)=>{
     req.logout();
     res.redirect("/");
 });
+
 
 module.exports = router;
